6d129746e235e9fafede4ef1ffd426c8474dfa93
dlichtistw <dlichtistw@gmx.de>
2019-06-26 22:56:02 +0200

    Sorting functions

--- anlagen.js
+++ anlagen.js
@@ -16,17 +16,27 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 /*
+ * Write some text to the console log.
+ */
+function log(t) {
+    return console.log("[STSext] " + t);
+}
+
+/*
  * Checks if the region tables are present.
  */
 function checkTablePresent() {
     return document.getElementById("anlagen") != null;
 }
 
+/*
+ * Return an array containing all region tables.
+ */
 function getRegionTables() {
     var tables = [];
     
     var container = document.getElementById("anlagen");
-    var re = /r\d+/i;
+    var re = /r(\d+|g)/i;
     for (i in container.children) {
         c = container.children[i];
         if (c.id && c.id.match(re) != null) {
@@ -43,28 +53,35 @@ function getRegionTables() {
 function insertGlobalRegion() {
     var tables = getRegionTables();    
     if (tables.length == 0) {
-        console.log("[STStools] No region tables found.");
+        log("No region tables found.");
         return false;
     }
     
     var table = tables[0].cloneNode(true);
     table.id = "rg_table";
-    var rows = table.getElementsByTagName("tr");
+
+    var tbody = table.getElementsByTagName("tbody")[0];
+    var rows = tbody.getElementsByTagName("tr");
 
     if (rows.length == 0) {
-        console.log("[STStools] No region entries found.");
+        log("No region entries found.");
         return false;
     }
     
-    while (rows.length > 2) {
-        rows[2].parentNode.removeChild(rows[2]);
-    }
+    var thead = document.createElement("thead");
+
+    var th1 = rows[0].cloneNode(true);
+    th1.firstChild.textContent = "Globale Übersicht";
+    thead.appendChild(th1);
+
+    var th2 = rows[1].cloneNode(true);
+    th2.firstChild.textContent = "Erzeugt durch STS Tools";
+    thead.appendChild(th2);
     
-    if (rows.length >= 1) {
-        rows[0].firstChild.textContent = "Globale Übersicht";
-    }
-    if (rows.length >= 2) {
-        rows[1].firstChild.textContent = "Erzeugt durch STS Tools";
+    table.insertBefore(thead, tbody);
+    
+    while (tbody.hasChildNodes()) {
+        tbody.removeChild(tbody.firstChild);
     }
     
     var div = tables[0].parentNode;
@@ -76,6 +93,9 @@ function insertGlobalRegion() {
     return true;
 }
 
+/*
+ * Copy entries from region tables and add them to the global region table.
+ */
 function fillGlobalRegion() {
     var tables = getRegionTables();
     var gt = document.getElementById("rg_table").getElementsByTagName("tbody")[0];
@@ -98,15 +118,15 @@ function toggleGlobalRegion() {
     rg = document.getElementById("rg");
     
     if (rg.style.display.match(/block/i)) {
-        rg.style.display = "none";
         for (i in tables) {
             tables[i].parentNode.style.display = "block";
         }
+        rg.style.display = "none";
     } else {
-        rg.style.display = "block";
         for (i in tables) {
             tables[i].parentNode.style.display = "none";
         }
+        rg.style.display = "block";
     }
 }
 
@@ -116,13 +136,13 @@ function toggleGlobalRegion() {
 function addAllAll() {
     var navbar = document.getElementById("URegionNavigation");
     if (navbar == null) {
-        console.log("[STStools] Navigation bar not found.");
+        log("Navigation bar not found.");
         return false;
     }
     
     var template = navbar.getElementsByTagName("td")[0];
     if (template == null) {
-        console.log("[STStools] Navigation template not found.");
+        log("Navigation template not found.");
         return false;
     }
     
@@ -140,31 +160,157 @@ function addAllAll() {
 }
 
 /*
- * Install sortable global overview.
+ * Add a global overview.
  */
-function sortableOverview() {
+function addOverview() {
     if (!checkTablePresent()) {
-        console.log("[STStools] Region overview not found. Are you using the classical view?");
+        log("Region overview not found. Are you using the classical view?");
         return false;
     }
     
     if (!addAllAll()) {
-        console.log("[STStools] Failed to insert navigation item.");
+        log("Failed to insert navigation item.");
         return false;
     }
     
     if (!insertGlobalRegion()) {
-        console.log("[STStools] Failed to insert global region.");
+        log("Failed to insert global region.");
         return false;
     }
     
     if (!fillGlobalRegion()) {
-        console.log("[STStools] Failed to fill global region table.");
+        log("Failed to fill global region table.");
         return false;
     }
     
-    console.log("[STStools] Installed global overview.");
+    log("Installed global overview.");
     return true;
 }
 
-sortableOverview();
+/*
+ * Creates an <a> node with some text.
+ */
+function createActionLink(text) {
+    l = document.createElement("a");
+    l.appendChild(document.createTextNode(text));
+    return l;
+}
+
+/*
+ * Prepare a table for sorting.
+ */
+function makeSortable(table) {
+    var ns = document.createElement("th");
+    var nsl = createActionLink("sortieren");
+    nsl.addEventListener("click", function(e) {
+        tg = e.target;
+        if (tg.classList.contains("sort_asc")) {
+            sortTable(table, function(a, b) { return compareName(a, b, true) });
+            tg.classList.remove("sort_asc");
+            tg.classList.add("sort_desc");
+        } else {
+            sortTable(table, function(a, b) { return compareName(a, b, false) });
+            tg.classList.remove("sort_desc");
+            tg.classList.add("sort_asc");
+        }
+    });
+    ns.appendChild(nsl);
+    
+    var ds = document.createElement("th");
+    var dsl = createActionLink("sortieren");
+    dsl.addEventListener("click", function(e) {
+        tg = e.target;
+        if (tg.classList.contains("sort_asc")) {
+            sortTable(table, function(a, b) { return compareGrade(a, b, true) });
+            tg.classList.remove("sort_asc");
+            tg.classList.add("sort_desc");
+        } else {
+            sortTable(table, function(a, b) { return compareGrade(a, b, false) });
+            tg.classList.remove("sort_desc");
+            tg.classList.add("sort_asc");
+        }
+    });
+    ds.appendChild(dsl);
+    
+    var shead = document.createElement("tr");
+    shead.appendChild(ns);
+    shead.appendChild(document.createElement("th"));
+    shead.appendChild(document.createElement("th"));
+    shead.appendChild(ds);
+    
+    table.getElementsByTagName("thead")[0].appendChild(shead);
+    
+    return true;
+}
+
+/*
+ * Compare two region list rows by their site's name.
+ */
+function compareName(row1, row2, reverse=false) {
+    if (reverse) {
+        return compareName(row2, row1, !reverse);
+    }
+
+    return row1.firstChild.textContent > row2.firstChild.textContent;
+}
+
+/*
+ * Compare two region list rows by their site's grading.
+ */
+function compareGrade(row1, row2, reverse=false) {
+    g1 = Number.parseFloat(row1.lastChild.textContent);
+    if (Number.isNaN(g1)) {
+        return true;
+    }
+    
+    g2 = Number.parseFloat(row2.lastChild.textContent);
+    if (Number.isNaN(g2)) {
+        return false;
+    }
+    
+    if (reverse) {
+        return compareGrade(row2, row1, !reverse);
+    }
+    
+    return g1 > g2;
+}
+
+/*
+ * Sort the rows in a table's tbody using a custom comparison method.
+ */
+function sortTable(table, comp) {
+    var tbody = table.getElementsByTagName("tbody")[0];
+    var rows = Array.from(tbody.getElementsByTagName("tr"));
+    
+    while (tbody.hasChildNodes()) {
+        tbody.removeChild(tbody.firstChild);
+    }
+    
+    rows.sort(comp);
+    
+    for (i in rows) {
+        tbody.appendChild(rows[i]);
+    }
+}
+
+/*
+ * Make the global site list sortable.
+ */
+function makeGlobalSortable() {
+    var table = document.getElementById("rg_table");
+    if (table == null) {
+        log("Global region table not found.");
+        return false;
+    }
+    
+    if (!makeSortable(table)) {
+        log("Failed to make global sortable.");
+        return false;
+    }
+    
+    log("Global is now sortable.");
+    return true;
+}
+
+addOverview();
+makeGlobalSortable();
